c3e7aa0d8c6714041a50be3b7e34f6f175e3f238
joaovitordev <jv17fc@gmail.com>
2021-04-05 21:40:55 -0300

    criando tela de erros de tela azul e fazendo navegação entre telas

--- components/BlueScreen/index.js
+++ components/BlueScreen/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import { ScrollView, StatusBar, TouchableOpacity } from 'react-native';
 
 import {
-  Container, Title, Card,  CardTitle, CardSubtitle, CardImage, Button
+  Container,
+  Header,
+  HeaderTitle,
+  BackButton,
+  ErrorListCard,
+  ErrorListCardTitle, 
+  ErrorListCardSolution,
 } from './styles';
 
-import blueScreeen from '../../assets/blue-screen.png';
-import bip from '../../assets/bell.png';
-import hardware from '../../assets/cpu.png';
-import software from '../../assets/content.png';
+import erros from '../../api/blue-screeen.json';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Homepage({navigation}) {
+
   return (
-    <Container>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </Container>
+    <ScrollView>
+      <StatusBar backgroundColor="#121212" barStyle="light-content" />
+      <Container>
+
+        <Header>
+        <TouchableOpacity style={{ display: 'contents' }} onPress={() => navigation.goBack()}>
+          <Icon name="arrow-back" size={30} color="#FFF" />
+        </TouchableOpacity>
+          <HeaderTitle>Erros de tela azul</HeaderTitle>
+        </Header>
+
+        {/* <Button title="Go back" onPress={() => navigation.goBack()} /> */}
+        
+        {erros.map(erro => (
+          <ErrorListCard key={erro.id}>
+            <ErrorListCardTitle> {erro.title} </ErrorListCardTitle>
+            <ErrorListCardSolution> {erro.solution} </ErrorListCardSolution>
+          </ErrorListCard>
+        ))}
+      </Container>
+    </ScrollView>
   );
 }
--- components/BlueScreen/styles.js
+++ components/BlueScreen/styles.js
@@ -7,4 +7,42 @@ export const Container = styled.View`
   height: 100%;
 `;
 
-export const Button = styled.Button``;
+export const Header = styled.View`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const BackButton = styled.Image``;
+
+export const HeaderTitle = styled.Text`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const ErrorListCard = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  background-color: #181818;
+  border-radius: 15px;
+  padding: 15px 5px;
+  margin-bottom: 20px;
+`;
+
+export const ErrorListCardTitle = styled.Text`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const ErrorListCardSolution = styled.Text`
+  color: #9c9c9c;
+  font-size: 14px;
+  text-align: center;
+`;
