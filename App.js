85d135aacdb24d426b71902d1b3455bbdc9639bf
joaovitordev <jv17fc@gmail.com>
2021-04-06 21:08:45 -0300

    criando tela de bips

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import Homepage from './components/Homepage';
 import BlueScreen from './components/BlueScreen';
+import Bips from './components/Bips';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ function MyStack() {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Homepage" component={Homepage} />
       <Stack.Screen name="BlueScreen" component={BlueScreen} />
+      <Stack.Screen name="Bips" component={Bips} />
     </Stack.Navigator>
   );
 }
