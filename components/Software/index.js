70ed03718f1b6b4674857a9850b9c7b3124f096d
joaovitordev <jv17fc@gmail.com>
2021-04-11 16:31:25 -0300

    tela de software criada

--- components/Software/index.js
+++ components/Software/index.js
@@ -2,19 +2,59 @@ import React from 'react';
 import { ScrollView, StatusBar, TouchableOpacity } from 'react-native';
 
 import {
-
+    Container,
+    HeaderTitle,
+    IconText,
+    ErrorListCard,
+    ErrorListCardSolution,
+    ErrorListCardAdditionalInformation,
+    ErrorListCardErro,
+    ErrorListCardExplication 
 } from './styles';
 
-import erros from '../../api/blue-screeen.json';
+import erros from '../../api/software.json';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import error from '../../assets/error.png';
+import information from '../../assets/information.png';
+import solution from '../../assets/solution.png';
+import explication from '../../assets/explication.png';
 
 export default function Software({navigation}) {
 
   return (
     <ScrollView>
       <StatusBar backgroundColor="#121212" barStyle="light-content" />
-     
+        <Container>
+            <TouchableOpacity style={{ display: 'contents' }} onPress={() => navigation.goBack()}>
+                <Icon name="arrow-back" size={30} color="#FFF" />
+            </TouchableOpacity>
+            <HeaderTitle>Erros de Software</HeaderTitle>
+
+            {erros.map(erro => (
+                <ErrorListCard key={erro.id}>
+                    <ErrorListCardErro>
+                        <IconText source={erro.erro ? error : ''} />
+                        { erro.erro }
+                    </ErrorListCardErro>
+
+                    <ErrorListCardAdditionalInformation>
+                        <IconText source={erro.additionalInformation ? information : ''} />
+                        { erro.additionalInformation }
+                    </ErrorListCardAdditionalInformation>
+
+                    <ErrorListCardSolution>
+                        <IconText source={erro.solution ? solution : ''} />
+                        { erro.solution }
+                    </ErrorListCardSolution>
+                    <ErrorListCardExplication>
+                        <IconText source={erro.explication ? explication : ''} />
+                        { erro.explication }
+                    </ErrorListCardExplication>
+                </ErrorListCard>
+            ))}
+        </Container>
     </ScrollView>
   );
 }
---
+++ components/Software/styles.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  padding: 40px 0 30px;
+  background-color: #121212;
+  height: 100%;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const ErrorListCard = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  background-color: #181818;
+  border-radius: 15px;
+  padding: 15px 5px;
+  margin-bottom: 20px;
+`;
+
+export const ErrorListCardErro = styled.Text`
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+`;
+
+export const ErrorListCardAdditionalInformation = styled.Text`
+    color: #9c9c9c;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+`;
+
+export const ErrorListCardSolution = styled.Text`
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+    justify-content: space-around;
+`;
+
+export const ErrorListCardExplication = styled.Text`
+    color: #9c9c9c;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+`;
+
+export const IconText = styled.Image`
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+`;
